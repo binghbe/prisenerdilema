55f1bb5fbceb8e959244a3d0a17e967cf9cb19e8
bear <binghbe@whs.school.nz>
2021-05-28 15:15:29 +1200

    Initial sharing of project

---
+++ V1_0.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class V1_0 here.
+ *version 1 it will have the lay out and some varibels
+ * @author (bear)
+ * @version (V1.0  28/5/2021)
+ */
+public class V1_0
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class V1_0
+     */
+    public V1_0()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
