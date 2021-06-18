a791bd233c892fff369144fb9c4a472e4d0ec69f
binghbe <84422280+binghbe@users.noreply.github.com>
2021-06-18 15:15:10 +1200

    Add files via upload

---
+++ priserdilema/V1_0.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class V1_0 here.
+ *version 1 it will have the lay out and some varibels
+ * @author (bear)
+ * @version (V1.0  28/5/2021)
+ */
+public class V1_0
+{
+    
+    private int x;
+    int roundnumber =0;
+    int roundsplayed=1;
+    int roundsanswers []= new int [25];
+
+    /**
+     * Constructor for objects of class V1_0
+     */
+    public V1_0()
+    {
+        System.out.println("                    round"+roundnumber);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("rounds min= 5 max= 25");
+        System.out.println("");
+        System.out.println("past "+roundsplayed+ " rounds");
+        System.out.println("");
+    }
+
+
+}
